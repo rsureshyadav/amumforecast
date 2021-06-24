f322953768b4dc27f16d847bd47b3c65f4c4f9cf
AMUMTrade <rsureshyadav@gmail.com>
2021-06-24 16:38:58 +0530

    initial  df completed

--- notebooks/Users/suresh@jaiyachitraoutlook.onmicrosoft.com/ingestion.py
+++ notebooks/Users/suresh@jaiyachitraoutlook.onmicrosoft.com/ingestion.py
@@ -1,2 +1,15 @@
 # Databricks notebook source
-print("Hello World")
+pip install nsetools
+
+# COMMAND ----------
+
+from nsetools import Nse
+import json
+
+import pandas as pd    
+
+nse = Nse()
+all_stock_codes = nse.get_stock_codes()
+top_gainers = nse.get_top_gainers()
+df = pd.DataFrame(top_gainers)
+df
