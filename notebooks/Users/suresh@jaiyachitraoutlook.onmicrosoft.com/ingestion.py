b75f7543b22a1dc07914f317e9142cccb1803683
AMUMTrade <rsureshyadav@gmail.com>
2021-06-25 00:46:31 +0530

    final code

--- notebooks/Users/suresh@jaiyachitraoutlook.onmicrosoft.com/ingestion.py
+++ notebooks/Users/suresh@jaiyachitraoutlook.onmicrosoft.com/ingestion.py
@@ -61,10 +61,102 @@ df1
 
 # COMMAND ----------
 
+import re
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd  
 
-url = "https://www.moneycontrol.com/india/stockpricequote/"
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html5lib')
-print(soup.prettify())
+def getStkOverview(soup):
+  Price = soup.find("td", text="VWAP").find_next_sibling("td").text
+  Volume = soup.find("td", text="Volume").find_next_sibling("td").text
+  AvgVolume = soup.find("td", text="20D Avg Volume").find_next_sibling("td").text
+  eps = soup.find("td", text="TTM EPS").find_next_sibling("td").text
+  pe = soup.find("td", text="TTM PE").find_next_sibling("td").text
+  Revenue = soup.find("td", text="Revenue").find_next_sibling("td").text
+  NetProfit = soup.find("td", text="NetProfit").find_next_sibling("td").text
+  OperatingProfit = soup.find("td", text="OperatingProfit").find_next_sibling("td").text
+  Moving_Averages = soup.find("td", text="Moving Averages").find_next_sibling("td").text
+  Technical_Indicators = soup.find("td", text=" Technical Indicators").find_next_sibling("td").text
+  Moving_Averages_Crossovers = soup.find("td", text="Moving Averages Crossovers").find_next_sibling("td").text
+  stk_concat=Price+";"+Volume+";"+","+AvgVolume+";"+eps+";"+pe+";"+Revenue+";"+NetProfit+";"+OperatingProfit+";"+Moving_Averages+";"+Technical_Indicators+";"+Moving_Averages_Crossovers
+  stk_concat=stk_concat.replace(',','')
+  stk_concat=stk_concat.replace(';',',')
+  return stk_concat
+
+  
+url = 'https://www.moneycontrol.com/stocks/marketstats/nsehigh/index.php'
+
+reqs = requests.get(url)
+soup = BeautifulSoup(reqs.content, 'html.parser')
+tbl_lst=[]
+for i in soup.find_all('span',{'class':'gld13 disin'}):
+  link = i.find('a',href=True)
+  title = links.get('title')
+  if link is None:
+    continue
+  item=link['title'] +","+ link['href']
+  items = item.split(',')
+  tbl_lst.append([items[0], items[1]])
+res = []
+[res.append(x) for x in tbl_lst if x not in res]
+df2 = pd.DataFrame(res)
+df2.columns =['Name', 'Url']
+
+stk_lst=[]
+for index, row in df2.iterrows():
+  print(str(index)+">>>"+row['Url'])
+  stk_reqs = requests.get(row['Url'])
+  stk_soup = BeautifulSoup(stk_reqs.text, 'html.parser')
+  stk_item=getStkOverview(stk_soup)
+  stk_items = stk_item.split(',')
+  stk_lst.append([row['Name'],stk_items[0], stk_items[1], stk_items[2], stk_items[3], stk_items[4], stk_items[5], stk_items[6], stk_items[7], stk_items[8], stk_items[9], stk_items[10],row['Url']])
+df3 = pd.DataFrame(stk_lst)
+df3.columns =['Name','Price', 'Volume', 'AvgVolume', 'eps', 'pe', 'Revenue', 'NetProfit', 'OperatingProfit', 'Moving_Averages', 'Technical_Indicators','Moving_Averages_Crossovers','Url']
+sdf=spark.createDataFrame(df3)
+display(sdf)
+
+# COMMAND ----------
+
+import re
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd  
+
+url = 'https://www.moneycontrol.com/india/stockpricequote/electricals/bharatelectronics/BE03'
+
+reqs = requests.get(url)
+soup = BeautifulSoup(reqs.text, 'html.parser')
+tbl_lst = []
+Price = soup.find("td", text="VWAP").find_next_sibling("td").text
+print("Price : "+Price)
+
+Volume = soup.find("td", text="Volume").find_next_sibling("td").text
+print("Today Volume : "+Volume)
+
+AvgVolume = soup.find("td", text="20D Avg Volume").find_next_sibling("td").text
+print("20D Avg Volume : "+AvgVolume)
+
+eps = soup.find("td", text="TTM EPS").find_next_sibling("td").text
+print("TTM EPS : "+eps)
+
+pe = soup.find("td", text="TTM PE").find_next_sibling("td").text
+print("TTM PE : "+pe)
+
+Revenue = soup.find("td", text="Revenue").find_next_sibling("td").text
+print("Revenue : "+Revenue)
+
+NetProfit = soup.find("td", text="NetProfit").find_next_sibling("td").text
+print("NetProfit : "+NetProfit)
+
+OperatingProfit = soup.find("td", text="OperatingProfit").find_next_sibling("td").text
+print("OperatingProfit : "+OperatingProfit)
+
+Moving_Averages = soup.find("td", text="Moving Averages").find_next_sibling("td").text
+print("Moving Averages : "+Moving_Averages)
+
+Technical_Indicators = soup.find("td", text=" Technical Indicators").find_next_sibling("td").text
+print("Technical Indicators : "+Technical_Indicators)
+
+Moving_Averages_Crossovers = soup.find("td", text="Moving Averages Crossovers").find_next_sibling("td").text
+print("Moving Averages Crossovers : "+Moving_Averages_Crossovers)
+
