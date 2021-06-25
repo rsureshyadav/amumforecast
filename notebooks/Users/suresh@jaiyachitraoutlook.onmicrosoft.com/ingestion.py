43fe431d332b363cffeb3e2ae5364ca095e8baae
AMUMTrade <rsureshyadav@gmail.com>
2021-06-25 15:35:15 +0530

    v1.0-swot

--- notebooks/Users/suresh@jaiyachitraoutlook.onmicrosoft.com/ingestion.py
+++ notebooks/Users/suresh@jaiyachitraoutlook.onmicrosoft.com/ingestion.py
@@ -142,52 +142,79 @@ df4 = df4[df4['Volume'] > 100000 ]
 df4 = df4[df4['20DayAvgVolume'] > 100000 ] 
 df4 = df4[df4['MktCap'] > 100 ] 
 df4 = df4[df4['eps'] > 0 ] 
-df4
+
 
 
 # COMMAND ----------
 
-import re
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd  
+swot_metric_lst=[]
+for index, row in df4.iterrows():
+  url_lst = row['Url'].split("/")
+  fnl_url = 'https://www.moneycontrol.com/swot-analysis/'+url_lst[len(url_lst)-2]+'/'+url_lst[len(url_lst)-1]+'/strength'
+  reqs = requests.get(fnl_url)
+  soup = BeautifulSoup(reqs.content, 'html.parser')
+  swot_lst=[]
+  for i in soup.find_all('div',{'class':'swot_count'}):
+    swot_lst.append([i.text])
+  for item in swot_lst:
+    str_item = str(item)
+    str_item = str_item.replace('\\n', '')
+    str_item = [character for character in str_item if character.isalnum()]
+    str_item = "".join(str_item)
+    if "Strengths" in str_item:
+      Strengths = str_item.replace("Strengths",'')
+      #swot_fnl_lst.append([Strengths])
+      #print(Strengths)
+    elif "Weaknesses" in str_item:
+      Weaknesses = str_item.replace("Weaknesses",'')
+      #swot_fnl_lst.append([Weaknesses])
+      #print(Weaknesses)
+    elif "Opportunities" in str_item:
+      Opportunities = str_item.replace("Opportunities",'')
+      #swot_fnl_lst.append([Opportunities])
+      #print(Opportunities)
+    elif "Threats" in str_item:
+      Threats = str_item.replace("Threats",'')
+      #swot_fnl_lst.append([Threats])
+  swot_metric_lst.append([row['Name'],row['Price'],Strengths,Weaknesses,Opportunities,Threats,row['Url']])	
+df5 = pd.DataFrame(swot_metric_lst)
+df5.columns =['Name','Price','Strengths','Weaknesses','Opportunities','Threats','Url']
+sdf1=spark.createDataFrame(df5)
+display(sdf1)
 
-url = 'https://www.moneycontrol.com/india/stockpricequote/power-transmissionequipment/a2zinfraengineering/AME02'
+# COMMAND ----------
 
+#url = 'https://www.moneycontrol.com/india/stockpricequote/transportlogistics/allcargologistics/AGL02'
+#url = 'https://www.moneycontrol.com/swot-analysis/allcargologistics/AGL02/strength'
+url_lst = url.split("/")
+fnl_url = 'https://www.moneycontrol.com/swot-analysis/'+url_lst[len(url_lst)-2]+'/'+url_lst[len(url_lst)-1]+'/strength'
 reqs = requests.get(url)
-soup = BeautifulSoup(reqs.text, 'html.parser')
-print(soup.find_all("td"))
-#tbl_lst = []
-#Price = soup.find("td", text="VWAP").find_next_sibling("td").text
-#print("Price : "+Price)
-#
-#Volume = soup.find("td", text="Volume").find_next_sibling("td").text
-#print("Today Volume : "+Volume)
-#
-#AvgVolume = soup.find("td", text="20D Avg Volume").find_next_sibling("td").text
-#print("20D Avg Volume : "+AvgVolume)
-#
-#eps = soup.find("td", text="TTM EPS").find_next_sibling("td").text
-#print("TTM EPS : "+eps)
-#
-#pe = soup.find("td", text="TTM PE").find_next_sibling("td").text
-#print("TTM PE : "+pe)
-#
-#Revenue = soup.find("td", text="Revenue").find_next_sibling("td").text
-#print("Revenue : "+Revenue)
-#
-#NetProfit = soup.find("td", text="NetProfit").find_next_sibling("td").text
-#print("NetProfit : "+NetProfit)
-#
-#OperatingProfit = soup.find("td", text="OperatingProfit").find_next_sibling("td").text
-#print("OperatingProfit : "+OperatingProfit)
-#
-#Moving_Averages = soup.find("td", text="Moving Averages").find_next_sibling("td").text
-#print("Moving Averages : "+Moving_Averages)
-#
-#Technical_Indicators = soup.find("td", text=" Technical Indicators").find_next_sibling("td").text
-#print("Technical Indicators : "+Technical_Indicators)
-#
-#Moving_Averages_Crossovers = soup.find("td", text="Moving Averages Crossovers").find_next_sibling("td").text
-#print("Moving Averages Crossovers : "+Moving_Averages_Crossovers)
+soup = BeautifulSoup(reqs.content, 'html.parser')
+swot_lst=[]
+for i in soup.find_all('div',{'class':'swot_count'}):
+  swot_lst.append([i.text])
+print(swot_lst)
+swot_fnl_lst=[]
+for item in swot_lst:
+  str_item = str(item)
+  str_item = str_item.replace('\\n', '')
+  str_item = [character for character in str_item if character.isalnum()]
+  str_item = "".join(str_item)
+  if "Strengths" in str_item:
+    Strengths = str_item.replace("Strengths",'')
+    swot_fnl_lst.append([Strengths])
+    #print(Strengths)
+  elif "Weaknesses" in str_item:
+    Weaknesses = str_item.replace("Weaknesses",'')
+    swot_fnl_lst.append([Weaknesses])
+    #print(Weaknesses)
+  elif "Opportunities" in str_item:
+    Opportunities = str_item.replace("Opportunities",'')
+    swot_fnl_lst.append([Opportunities])
+    #print(Opportunities)
+  elif "Threats" in str_item:
+    Threats = str_item.replace("Threats",'')
+    swot_fnl_lst.append([Threats])
+    #print(Threats)
+
 
